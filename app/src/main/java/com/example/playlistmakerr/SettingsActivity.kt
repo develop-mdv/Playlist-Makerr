80b32c16600f4eca5beba5c884271af46358b955
mayboroda.d <mayboroda.d@granit-concern.ru>
2025-12-12 02:25:45 +0300

    Дз спринт 8

--- app/src/main/java/com/example/playlistmakerr/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmakerr/SettingsActivity.kt
@@ -1,18 +1,16 @@
 package com.example.playlistmakerr
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val backButton: ImageButton? = findViewById(R.id.btn_back)
-        backButton?.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
+        val toolbar: MaterialToolbar = findViewById(R.id.toolbar)
+        toolbar.setNavigationOnClickListener {
             finish()
         }
     }
