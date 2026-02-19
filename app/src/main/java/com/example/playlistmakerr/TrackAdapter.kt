3280ac7c29e568f1b54a4ddad2b2fbbc23fbb491
mayboroda.d <mayboroda.d@granit-concern.ru>
2026-02-19 17:25:35 +0300

    Дз спринт 10

---
+++ app/src/main/java/com/example/playlistmakerr/TrackAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.playlistmakerr
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+
+class TrackAdapter(
+    private val tracks: List<Track>
+) : RecyclerView.Adapter<TrackAdapter.TrackViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_track, parent, false)
+        return TrackViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
+        holder.bind(tracks[position])
+    }
+
+    override fun getItemCount(): Int = tracks.size
+
+    class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val coverImageView: ImageView = itemView.findViewById(R.id.iv_cover)
+        private val trackNameTextView: TextView = itemView.findViewById(R.id.tv_track_name)
+        private val artistNameTextView: TextView = itemView.findViewById(R.id.tv_artist_name)
+        private val trackTimeTextView: TextView = itemView.findViewById(R.id.tv_track_time)
+
+        fun bind(track: Track) {
+            trackNameTextView.text = track.trackName
+            artistNameTextView.text = track.artistName
+            trackTimeTextView.text = track.trackTime
+
+            val radius = itemView.resources.getDimensionPixelSize(R.dimen.track_cover_corner_radius)
+
+            Glide.with(itemView)
+                .load(track.artworkUrl100)
+                .transform(CenterCrop(), RoundedCorners(radius))
+                .placeholder(R.drawable.track_cover_placeholder)
+                .error(R.drawable.track_cover_placeholder)
+                .into(coverImageView)
+        }
+    }
+}
+
